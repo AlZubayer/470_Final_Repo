26b35f8624b3c818bb3c93602fe7090817338d01
Saumik Das Turja <saumik.das.turja@gmail.com>
2025-01-06 00:17:31 +0600

    feat: implement reporting and feedback system for chat interactions and profiles
    Chat Interaction Reporting: Enables users to report inappropriate or concerning chat interactions, bringing them to the admin team's attention for review and action.
    Profile Reporting: Allows users to report other user profiles with a clear reason, ensuring admins have sufficient context to evaluate each case effectively.
    Report Feedback: Provides users with updates on the status of their reports, including actions taken, enhancing transparency and user satisfaction.

--- backend/controllers/All.js
+++ backend/controllers/All.js
@@ -2,6 +2,7 @@ import bcrypt from "bcrypt";
 import User from "../models/userModel.js";
 import jwt from "jsonwebtoken";
 import nodemailer from 'nodemailer';
+import Report from '../models/Report.js';
 
 export async function verifyEmail(req, res) {
     try {
@@ -208,3 +209,21 @@ export async function login(request, response) {
         return response.status(500).json({ message: "Internal server error." });
     }
 }
+
+
+export const createReport = async (req, res) => {
+    console.log("Received request to create a report");
+    try {
+        const { userEmail, type, details } = req.body;
+        const newReport = new Report({
+            userEmail,
+            type,
+            details
+        });
+        console.log("New report created:", newReport);
+        await newReport.save();
+        res.status(201).json(newReport);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
