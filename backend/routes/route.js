d4a5bd9e35eed983183ab18d7cee5bdb54f1e486
raqin31 <mohammed.raqin.rahman@g.bracu.ac.bd>
2025-01-06 01:21:26 +0600

    fix: adding all the routes

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { signup, login,  verifyEmail, confirmEmail, sendPasswordResetEmail, resetPassword, questions} from "../controllers/All.js"
+import { signup, login, profile, updateProfile, getPosts, createPost, updatePost, deletePost, upvotePost, addComment, getComments, deleteComment, getReports, createReport, companion, companions, verifyEmail, confirmEmail, sendPasswordResetEmail, resetPassword, questions, uploadProfilePicture, getProfilePicture} from "../controllers/All.js"
 import { authenticateToken } from "../config/middlewares.js"
 
 const router = express.Router()
@@ -8,6 +8,15 @@ router.post("/signup", signup)
 router.post("/login", login)
 router.get("/profile", authenticateToken, profile)
 router.put("/profile", authenticateToken, updateProfile)
+
+router.get('/posts', getPosts)
+router.post('/posts', authenticateToken, createPost);
+router.put('/posts/:id', authenticateToken, updatePost);
+router.delete('/posts/:id', authenticateToken, deletePost);
+router.post('/posts/:id/upvote', authenticateToken, upvotePost);
+router.post('/posts/:id/comments', authenticateToken, addComment);
+router.get('/posts/:id/comments', getComments);
+router.delete('/posts/:postId/comments/:commentId', authenticateToken, deleteComment);
 
 
 router.get('/reports', authenticateToken, getReports);
@@ -25,6 +34,5 @@ router.get('/questions',authenticateToken, questions)
 
 router.post('/upload-profile-picture', authenticateToken, uploadProfilePicture);
 router.get('/profile-picture', authenticateToken, getProfilePicture);
-
 
 export default router
