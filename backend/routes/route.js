ba97ab348d60a1abcca10949c95a3ef48c9da1b7
Shaiera Sultana Oishe <shaiera.sultana.oishe@g.bracu.ac.bd>
2025-01-06 00:41:05 +0600

    feat: Enable forum posting, commenting, and upvoting for user engagement
    Added functionality for users to create posts in specific categories or fields.
    Implemented the ability for users to upload images to their posts for better expression and context.
    Enabled user engagement by allowing comments on posts and upvoting of helpful responses to foster community interaction and collaboration.

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -2,6 +2,20 @@ import express from "express"
 import { signup, login,  verifyEmail, confirmEmail, sendPasswordResetEmail, resetPassword} from "../controllers/All.js"
 import { authenticateToken } from "../config/middlewares.js"
 
+
+
+
+import express from "express"
+import { getPosts, createPost,  upvotePost, addComment,} from "../controllers/All.js"
+import { authenticateToken } from "../config/middlewares.js"
+
+
+router.get('/posts', getPosts)
+router.post('/posts', authenticateToken, createPost);
+router.post('/posts/:id/upvote', authenticateToken, upvotePost);
+router.post('/posts/:id/comments', authenticateToken, addComment);
+
+
 const router = express.Router()
 
 router.post("/signup", signup)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 import Signup from './pages/Signup'
 import Login from './pages/Login'
 import ResetPassword from './pages/ResetPassword'
+import Forum from './pages/Forum'
 
 import axios from 'axios'
 
@@ -120,6 +121,7 @@ const App = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/forum" element={<Forum />} />
         </Routes>
       </div>
     </Router>
