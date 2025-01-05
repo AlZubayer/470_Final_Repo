c68a3ec72b1770f84e508d488b2a41705de196b9
Saumik Das Turja <saumik.das.turja@gmail.com>
2025-01-05 23:51:42 +0600

    feat: Add member profile creation, email verification, and advice API integration
    Member Profile Creation: Added functionality to collect user information during account creation, including gender preference, problem areas (e.g., anxiety, depression, trauma), language, and age range to create a detailed profile.
    Email Verification: Implemented email verification for account registration and email changes, along with password reset functionality via email.
    External API Integration: Integrated the Advice API to display a unique and inspiring piece of advice to users each time they log in, ensuring a fresh experience on every visit.

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from "dotenv"
+import { connectDB } from './config/db.js';
+import router from './routes/route.js';
+import { createServer } from 'http';
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT ||  5001;
+
+const server = createServer(app);
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.use("",router)
+
+
+connectDB();    
+
+// Start Server
+server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
